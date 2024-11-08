cc7fb845fd957ea4691da3cbb1a0c5556d711634
melv20 <58644009+melvinrdn@users.noreply.github.com>
2024-11-08 08:51:42 +0100

    Added AutoAttCalib into the code (and also made it a different github)

---
+++ diagnostics/WaveplateCalib/AutoAttCalib/run_calibration.py
@@ -0,0 +1,15 @@
+import os
+import numpy as np
+from auto_att_calib import AutoAttCalib
+
+# Settings
+motor_id = '83837725'
+powermeter_id = 'USB0::0x1313::0x8078::P0045634::INSTR'
+stabilization_time = 1
+waveplate_name = 'wp_1'
+waveplate_path = os.path.join('../../../ressources/calibration', waveplate_name)
+angles_to_measure = np.linspace(0, 180, 181)
+
+# Run calibration
+autoattcalib = AutoAttCalib(motor_id, powermeter_id, stabilization_time=stabilization_time)
+autoattcalib.run(angles_to_measure, waveplate_name, waveplate_path)
