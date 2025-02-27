83e782eff478039080bd32f3c600465940b58928
Melvin <melvin.redon@icloud.com>
2025-02-27 13:43:49 +0100

    added second axis in temporal scan corresponding the the time. Fixed the colormap

---
+++ diagnostics/utils.py
@@ -0,0 +1,15 @@
+from matplotlib.colors import LinearSegmentedColormap
+
+def custom_cmap(segments=512):
+    colors = [
+        (1, 1, 1),  # white
+        (0, 0, 0.5),  # dark blue
+        (0, 0, 1),  # clearer blue
+        (0, 1, 1),  # turquoise
+        (0, 1, 0),  # green
+        (1, 1, 0),  # yellow
+        (1, 0.5, 0),  # orange
+        (1, 0, 0),  # red
+        (0.5, 0, 0)  # darker red
+    ]
+    return LinearSegmentedColormap.from_list('custom_cmap', colors, N=segments)
